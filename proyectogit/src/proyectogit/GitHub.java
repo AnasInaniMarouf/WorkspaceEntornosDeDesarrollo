c98fee5ef7fa433e0ba08e7a2c7cd4079cf02fc0
AnasInaniMarouf <ainamar635@g.educaand.es>
2024-10-25 13:54:02 +0200

    Version 3

---
+++ proyectogit/src/proyectogit/GitHub.java
@@ -0,0 +1,13 @@
+package proyectogit;
+
+public class GitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hola mundo");
+		System.out.print("Versión 2 fácil");
+
+	}
+
+}
